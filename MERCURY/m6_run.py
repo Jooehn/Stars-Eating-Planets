0930839db905f363c9b7fb4594add33a340323e1
jooehn@gmail.com <jooehn@gmail.com>
2019-05-11 14:39:53 +0200

    Adjusted for arbitrary 3J+2E runs

--- MERCURY/m6_run.py
+++ MERCURY/m6_run.py
@@ -7,23 +7,26 @@ Created on Fri Feb  1 11:19:24 2019
 """
 
 from subprocess import call
+from subprocess import Popen
 from m6_input import *
 from m6_output import *
 import pandas as pd
 import os
+import string
 
 ##################### Data entry #########################
 
 #Enter data and generate the input files. If smalldata is none, just call
 #the function without passing arguments to it
 
-big_names      = ['MERCURY','VENUS','EARTHMOO','MARS','JUPITER','SATURN',\
-                  'URANUS','NEPTUNE','PLUTO']
+big_names      = ['E1','E2','J1','J2','J3']
 small_names    = []
 
 all_names      = big_names+small_names
 
-#big_input(bigdata)
+bigdata = np.loadtxt('3J+2E.data',delimiter=',')
+
+rand_big_input(big_names,bigdata)
 #small_input(smalldata)
 small_input()
 
@@ -31,11 +34,6 @@ small_input()
 
 #rand_big_input(bigdata)
 #rand_small_input(smalldata)
-
-#We can also boost the mass of the big objects in the system by a factor alpha
-
-#alpha = 2
-#mass_boost(alpha)
 
 ###################### Simulations ##########################
 
@@ -48,7 +46,9 @@ N_big   = len(big_names)
 N_small = len(small_names)
 N_all   = len(all_names)
 
-N_sim = 1
+#We choose the number of simulations
+
+N_sim = 50
 
 #We define variables for strings that we want to find in the info.out file
 
@@ -56,131 +56,147 @@ frac_ec_int = 'Fractional energy change due to integrator:'
 frac_am_ch  = 'Fractional angular momentum change:'
 ejected     = 'ejected at'
 star_coll   = 'collided with central body at'
-rem_a       = 'removed due to small a'
-rem_ce      = 'removed due to an encounter with'
+coll        = 'was hit by'
 
-event_list  = [ejected,star_coll,rem_ce,rem_a]
+event_list  = [ejected,star_coll,coll]
 
 siminfo = np.zeros((N_sim,6))
 
 simdata     = []
 ce_simdata  = []
 
-#We also set up the duration of each simulation
+#We also set up the duration of each simulation. T is the total allowed time for
+#the integration while dt is the time allocated for every sub integration. After
+#dt we check if any planet has hit the host star. If so we terminate the integration
+#otherwhise, we extend the time by dt.
 
-T = 1000
-setup_stop_time(T)
+T   = 1e7
+dt  = 1e5
+setup_end_time(dt)
 
-#We also remove some files that we want to recreate
+#We also remove a file that we want to recreate
 
 call(['find',os.getcwd(),'-maxdepth','1','-type','f','-name','lossinfo.txt','-delete'])
+call(['find',os.getcwd(),'-maxdepth','1','-type','f','-name','siminfo.txt','-delete'])
 
 #We loop through each simulation and gather the information we need from it
 
-for i in range(N_sim):
+for k in range(N_sim):
     
-    N_runs = 0
-    
-    while True:
-    
-        #We then want to clear any output or dump files present in the working
-        #directory, which we can do via our terminal
-        bad_ext = ['*.dmp','*.tmp','*.aei','*.clo','*.out']        
-        for j in bad_ext:
-            call(['find',os.getcwd(),'-maxdepth','1','-type','f','-name',j,'-delete'])
+    simdata_sys     = []
+    ce_simdata_sys  = []
         
-        energy_change           = np.array([])
-        angmom_change           = np.array([])
-        star_collisions         = 0
-        ejections               = 0
-        collisions              = 0
-        removed                 = 0
+    #The following function randomizes the phase of the big bodies
+    rand_big_input(big_names,bigdata)
+    
+    #We then want to clear any output or dump files present in the working
+    #directory, which we can do via our terminal
+    bad_ext = ['*.dmp','*.tmp','*.aei','*.clo','*.out']        
+    for j in bad_ext:
+        call(['find',os.getcwd(),'-maxdepth','1','-type','f','-name',j,'-delete'])
+    
+    losslist                = []
+    energy_change           = np.array([])
+    angmom_change           = np.array([])
+    star_collisions         = 0
+    ejections               = 0
+    collisions              = 0
+    
+    #We now carry out our sub-integrations for which we check if we have had
+    #any star-planet collisions
+    t = 0
+    
+    while t < T:
         
         #We first perform the integration by calling MERCURY
         
-        call(['./mercury6'])
-        
-        #We then want to go through the info.out file, if one of our keywords
-        #appear, we note down the details of the input
+        call(['./mercury6'])    
         
         with open('info.out') as info:
             
-            for line in info:
-                
-                if frac_ec_int in line:
-                    ec = float(line.strip(frac_ec_int))
-                    energy_change = np.append(energy_change,ec)
-                    
-                elif frac_am_ch in line:
-                    amc = float(line.strip(frac_am_ch))
-                    angmom_change = np.append(angmom_change,amc)
-                    
-                #We then investigate if any special event has occured, if so
-                #we write this event to a file that can be used as a reference
-                #for post-analysis
-                
-                elif any([i in line for i in event_list]):
-                    
-                    if ejected in line:
-                        ejections += 1
-                    
-                    elif star_coll in line:
-                        star_collisions += 1
-                     
-                    elif any([rem_a in line,rem_ce in line]):
-                        removed += 1
-                
-                    with open('lossinfo.txt','a') as lossinfo:
-                        lossinfo.write('{}\t'.format(i)+line+'\n')
-                    
-        #We can now save the info we have extracted
-                    
-        survived = N_all-ejections-star_collisions-removed
-
-        #We only want to consider runs where the energy change due to the integrator
-        #is insignificantly small, if the energy change is larger than 1e-4 we
-        #therefore reiterate
-        
-        if abs(max(energy_change))<1e-4:
+            infolines = info.readlines()
             
-            #If all bodies survive, we extend the integration, else we move on and
-            #save the data
-    
-            if survived < N_all:
+        for line in infolines:
+            
+            if star_coll in line:
                 break
-            else:
-                N_runs +=1
-                print('No casualties in simulation {0}. Integrating for another {1} yr'.format(i,T))
-                extend_stop_time(T)
-                if N_runs == 3:
-                    print('All planets survived in simulation {}, even after integrating for an'.format(i)\
-                          +' extra {} yr'.format(3*T))
-                    break
+            
+        else:
+            extend_stop_time(dt)
+            t += dt
+            continue
+        break
+    
+    #We then want to go through the info.out file, if one of our keywords
+    #appear, we note down the details of the input
+    
+    with open('info.out') as info:
+        
+        for line in info:
+            
+            if frac_ec_int in line:
+                ec = float(line.strip(frac_ec_int))
+                energy_change = np.append(energy_change,ec)
+                
+            elif frac_am_ch in line:
+                amc = float(line.strip(frac_am_ch))
+                angmom_change = np.append(angmom_change,amc)
+                
+            #We then investigate if any special event has occured, if so
+            #we write this event to a file that can be used as a reference
+            #for post-analysis
+            
+            elif any([k in line for k in event_list]):
+                
+                if ejected in line:
+                    ejections += 1
+                
+                elif star_coll in line:
+                    star_collisions += 1
+                 
+                elif coll in line:
+                    collisions += 1
+            
+                losslist.append(line)
+                
+    #We can now save the info we have extracted
+                
+    survived = N_all-ejections-star_collisions-collisions
+    
+    with open('lossinfo.txt','a') as lossinfo:
+        
+        for loss in losslist:
+            lossinfo.write('{}\t'.format(k)+loss+'\n')
         
     #We now have to save our data into files, that later can be analysed
         
-    siminfo[i] = np.array([survived,star_collisions,ejections,removed,max(energy_change),
-                        max(angmom_change)])
-    
+    siminfo[k] = np.array([survived,star_collisions,ejections,collisions,max(abs(energy_change)),
+                            max((angmom_change))])
+        
     call(['./element6'])
     call(['./close6'])
-    
+        
     orb_data, ce_data = m6_read_output(all_names)            
+        
+    simdata_sys.append(orb_data)
+    ce_simdata_sys.append(ce_data)
+        
+    #Now the run for our system is complete and we have got the information
+    #we wanted. We save the simulation data in each 
     
-    simdata.append(orb_data)
-    ce_simdata.append(ce_data)
-    
-    #Now the run is complete and we have got the information we wanted
-    
-siminfo_pd = pd.DataFrame(siminfo,columns = ['Survived','Star Collisions','Ejections','Removed','dE','dL'])
+    simdata.append(simdata_sys)
+    ce_simdata.append(ce_simdata_sys)
 
 #With the help of pandas, we can create a nicely formatted file with all the info
 #we need to find possible outlier simulations
-    
-with open('siminfo.txt','w+') as simfile:
-    
-    simfile.write(siminfo_pd.__repr__())
 
+siminfo_pd = pd.DataFrame(siminfo,columns = ['Surv',\
+             'SCols','Eject','Cols','dE','dL'])
+
+with open('siminfo.txt','a') as simfile:
+
+    simfile.write(siminfo_pd.__repr__())
+    
 #We would also like to save all our data, which is efficiently done using a 
 #pickle dump
     
@@ -191,5 +207,13 @@ if any([len(i)!=0 for i in ce_simdata]):
 else:
     m6_save_data(simdata)
 #The data can then easily be extracted using m6_load_data()
+
+#We clear the rest of the files we do not need to keep things tidy
+
+#We then want to clear any output or dump files present in the working
+#directory, which we can do via our terminal
+bad_ext = ['*.dmp','*.tmp','*.aei','*.clo','*.out']        
+for j in bad_ext:
+    call(['find',os.getcwd(),'-maxdepth','1','-type','f','-name',j,'-delete'])
 
 print('My work here is done')
